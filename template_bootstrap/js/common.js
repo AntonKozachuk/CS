8b4e4c3f016235900e93d02585674979300ddc11
Anton <invulnarble@gmail.com>
2020-06-30 08:40:59 +0300

    basic template

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+	$(".mc_item_wrap ul").each(function() {
+		$(this).after("<div class='mc_item_wrap_af'></div>");
+	});
+	$(".mc_toggle").click(function() {
+		if($(this).parent().parent().children('ul').is(':visible')) {
+			$(this).parent().parent().children('ul').slideUp();
+			$(this).parent().parent().children('.mc_item_wrap_af').hide();
+		} else {
+			$('body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af').hide();
+			$('.mc_item_wrap').removeClass('active');
+			$(this).parent().parent().addClass('active');
+			$(this).parent().parent().children('ul').slideDown();
+			$(this).parent().parent().children('.mc_item_wrap_af').show(0);
+		}});
+
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
@@ -18,7 +34,7 @@ $(document).ready(function() {
 	//equalheight - одинаковая высота колонок
 	//Пример списка элементов:
 	//var eqElement = ".cat_container > div, .home_news > div"
-	var eqElement = ".element"
+	var eqElement = ".hi_item, .sb_content > div"
 	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
 	//Masked Input Plugin
@@ -40,7 +56,7 @@ $(document).ready(function() {
 	//Навигация по Landing Page
 	//$(".top_mnu") - это верхняя панель со ссылками.
 	//Ссылки вида <a href="#contacts">Контакты</a>
-	$(".top_mnu").navigation();
+	// $(".top_mnu").navigation();
 
 	//Добавляет классы дочерним блокам .block для анимации
 	//Документация: http://imakewebthings.com/jquery-waypoints/
